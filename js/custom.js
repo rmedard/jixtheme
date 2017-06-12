db153cbcd49dc19d737a64f7074516322eb0ef08
Medard <rmedard5@gmail.com>
2017-06-12 13:09:24 +0200

    EC

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,10 @@
 
     //$('select').selectBoxIt();
 
-    $('form#views-exposed-form-jobs-page-jobs-search > div > div.form-item-combine').addClass('col-lg-6 col-md-6');
-    $('form#views-exposed-form-jobs-page-jobs-search > div > div.form-item-combine > div.input-group').css('width', '100%');
-    $('form#views-exposed-form-jobs-page-jobs-search > div > div.form-item-field-employer-sector-value').addClass('col-lg-4 col-md-4');
-    $('form#views-exposed-form-jobs-page-jobs-search > div > div.form-actions').addClass('col-lg-2 col-md-2');
+    $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-combine').addClass('col-lg-6 col-md-6');
+    $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-combine > div.input-group').css('width', '100%');
+    $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-field-employer-sector-value').addClass('col-lg-4 col-md-4');
+    $('form#views-exposed-form-adverts-page-search-ads > div > div.form-actions').addClass('col-lg-2 col-md-2');
 
     $('#edit-submit-adverts').removeClass('btn-xs');
 
