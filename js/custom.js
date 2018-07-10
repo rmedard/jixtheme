4e250ac8817010629e1ac8829ff9dd7d2f4681cc
Medard <rmedard5@gmail.com>
2018-07-10 16:45:48 +0200

    EC

--- js/custom.js
+++ js/custom.js
@@ -3,37 +3,30 @@
  */
 (function ($, Drupal) {
 
-    //$('select').selectBoxIt();
-
     'use strict';
     Drupal.behaviors.awesome = {
         attach: function (context, settings) {
-            console.log(settings);
+
             if (settings.path.isFront) {
                 $('nav#block-jir-main-menu > ul.menu > li:first-child').addClass('active');
             }
+
+            $('form#views-exposed-form-adverts-page-search-ads input').addClass('input-sm');
+            $('form#views-exposed-form-adverts-page-search-ads div.input-group > span').addClass('input-sm');
+            $('form#views-exposed-form-adverts-page-search-ads select').addClass('input-sm');
+
+            $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-combine').addClass('col-lg-6 col-md-6');
+            $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-combine > div.input-group').css('width', '100%');
+            $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-field-employer-sector-value').addClass('col-lg-4 col-md-4');
+            $('form#views-exposed-form-adverts-page-search-ads > div > div.form-actions').addClass('col-lg-2 col-md-2');
+
+            $('div.field--name-field-application-form > a').addClass('btn btn-success btn-xs btn-block');
+
+            $('div.view-jobs-view > div.view-content > div.media > div:first-child').addClass('job-teaser-left col-md-2 col-sm-2 col-xs-2');
+            $('div.view-jobs-view > div.view-content > div.media > div:last-child').addClass('job-teaser-right col-md-10 col-sm-10 col-xs-10');
+
+            $('div#edit-field-job-company-name-wrapper').append("<div><a href='/node/add/employer'>Vous ne trouvez pas l'employeur? Cliquez ici.</a></div>");
         }
     };
 
-    // if ( window.location.pathname === '/' ) {
-    //     $('nav#block-jir-main-menu > ul.menu >
-    // li:first-child').addClass('active'); }
-
-    $('form#views-exposed-form-adverts-page-search-ads input').addClass('input-sm');
-    $('form#views-exposed-form-adverts-page-search-ads div.input-group > span').addClass('input-sm');
-    $('form#views-exposed-form-adverts-page-search-ads select').addClass('input-sm');
-
-    $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-combine').addClass('col-lg-6 col-md-6');
-    $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-combine > div.input-group').css('width', '100%');
-    $('form#views-exposed-form-adverts-page-search-ads > div > div.form-item-field-employer-sector-value').addClass('col-lg-4 col-md-4');
-    $('form#views-exposed-form-adverts-page-search-ads > div > div.form-actions').addClass('col-lg-2 col-md-2');
-
-    $('#edit-submit-adverts').removeClass('btn-xs');
-
-    $('div.field--name-field-application-form > a').addClass('btn btn-success btn-xs btn-block');
-
-    $('div.view-jobs-view > div.view-content > div.media > div:first-child').addClass('job-teaser-left col-md-2 col-sm-2 col-xs-2');
-    $('div.view-jobs-view > div.view-content > div.media > div:last-child').addClass('job-teaser-right col-md-10 col-sm-10 col-xs-10');
-
-    $('div#edit-field-job-company-name-wrapper').append("<div><a href='/node/add/employer'>Vous ne trouvez pas l'employeur? Cliquez ici.</a></div>");
 }(jQuery, Drupal));
