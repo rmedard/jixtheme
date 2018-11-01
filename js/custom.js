1bc615160496d357be98aa0e3bccd1a8587b146b
rmedard <00Derfine@5>
2018-11-01 15:58:21 +0100

    W10

--- js/custom.js
+++ js/custom.js
@@ -39,6 +39,31 @@
             if ($.trim(superFeaturedBox.text()) === '') {
                 superFeaturedBox.hide();
             }
+
+            if (isMobile.any()) {
+                $('nav#block-jobstabsmenu > ul.nav-pills').removeClass('nav-justified');
+            }
+        }
+    };
+
+    const isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
         }
     };
 
