b43388d9d567085a7e822f0f7ff8eec504361889
Medard Rebero <medard.rebero@gmail.com>
2019-05-16 01:54:29 +0200

    Win10

--- js/custom.js
+++ js/custom.js
@@ -72,9 +72,9 @@
                 }
             }
 
-            // $.fn.select2.defaults.set( "theme", "bootstrap" );
-            $(context).find('select#edit-field-employer-secteur').once(main).select2({theme: 'bootstrap'});
-            $(context).find('select#edit-field-job-categorie').once(main).select2({theme: 'bootstrap'});
+            $.fn.select2.defaults.set('theme', 'bootstrap');
+            // $(context).find('select#edit-field-employer-secteur').once(main).select2({theme: 'bootstrap'});
+            // $(context).find('select#edit-field-job-categorie').once(main).select2({theme: 'bootstrap'});
 
             const input = document.querySelector("#edit-field-job-contact-phone-number-0-value");
             window.intlTelInput(input, {initialCountry: 'bi', nationalMode: false});
